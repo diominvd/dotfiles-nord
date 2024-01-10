1cb53308c77680fef706d522075ac0a06181997d
diominvd <diomin.vd13@icloud.com>
2024-01-10 13:53:52 +0300

    Polybar Neofetch

---
+++ polybar/blocks/scripts/spp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+playerctl status 2> /dev/null 1> /dev/null
+
+if [ $? == 0 ]
+  then
+  	if [[ $(playerctl status) == 'Playing' ]]
+  	  then
+	    echo '  '
+	  else
+	    echo '  '
+	fi
+  else
+	echo '  '
+fi
